4953b6b0a789311757f3bdcc58cd1776919a29c8
System Administrator <root@it2106m.local>
2018-10-03 08:59:12 -0600

    login authentication update

--- config/passport.js
+++ config/passport.js
@@ -5,11 +5,18 @@ const User = mongoose.model('users');
 const keys = require('../config/keys');
 
 const opts = {};
-opts.jwtFormRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-    passport.use(new JwtStrategy(opts), (jwt_payload, done) => {
-        
-    })
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        User.findById(jwt_payload.id).then((user) => {
+            if(user) {
+                return done(null, user);
+            }
+            return done(null, false);
+        }).catch((error) => {
+            return console.log(error)
+        });
+    }));
 };
